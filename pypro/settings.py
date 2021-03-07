268083ba8ab77f3a9550499183fb147f58f03b9d
Rodrigo Usuy <rousuy@gmail.com>
2021-03-06 21:06:20 -0300

    Instalado biblioteca para otimização de Deploy em nuvem

--- pypro/settings.py
+++ pypro/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base',
 ]
@@ -125,10 +126,10 @@ USE_TZ = True
 # Configuração de Ambiente de Desenvolvimento
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 MEDIA_URL = '/media/'
 # Configurações caso o site proprietário permitir ou conter arquivos para upload do usuário.
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+COLLECTFAST_ENABLED = False
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
 # STORAGE CONFIGURATION IN S3 AWS
@@ -140,6 +141,7 @@ if AWS_ACCESS_KEY_ID:
     AWS_AUTO_CREATE_BUCKET = False
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
+    COLLECTFAST_ENABLED = True
     AWS_DEFAULT_ACL = 'private'
 
     # Statics Assets
